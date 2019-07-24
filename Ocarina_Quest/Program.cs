557a54f06852af9a08465e9ad5929f6879b41eea
stephan garrett <stephan.garrett@yahoo.com>
2019-07-24 19:11:59 -0400

    Update menu navigation

--- Ocarina_Quest/Program.cs
+++ Ocarina_Quest/Program.cs
@@ -9,153 +9,213 @@ using System.IO;
 using System.Net;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Links
 {
-	class Program
-	{
+    class Program
+    {
         private static List<Song> songs = new List<Song>();
-		private static List<Song> contSongs = new List<Song>();
-		private static string name = "";
-		private static string currentDirectory = Directory.GetCurrentDirectory();
-		private static DirectoryInfo directory = new DirectoryInfo(currentDirectory);
-		private static string fileName = Path.Combine(directory.FullName, "Songs.txt");
-		private static string contFile = Path.Combine(directory.FullName, "Character.txt");
+        private static List<Song> contSongs = new List<Song>();
+        private static string name = "";
+        private static string currentDirectory = Directory.GetCurrentDirectory();
+        private static DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+        private static string songFile = Path.Combine(directory.FullName, "Songs.txt");
+        private static string contFile = Path.Combine(directory.FullName, "Character.txt");
 
-		static void Main(string[] args)
-		{
-			songs = ReadSongs(fileName);
-			contSongs = ReadSongs(contFile);
-			RunMenu();
-		}
 
-		public static void RunMenu()
-		{
-			Console.WriteLine("");
-			Console.WriteLine("Welcome To Ocarina Quest!!");
-			Console.WriteLine("What would you like to do?");
-			Console.WriteLine("1) Create new player");
-			Console.WriteLine("2) Continue ");
+        static void Main(string[] args)
+        { 
+            // Assign and Read files to song lists
+            songs = ReadSongs(songFile);
+            contSongs = ReadSongs(contFile);
 
-			string runChoice = Console.ReadLine();
+            // Load run menu to start app
+            RunMenu();
+        }
 
-			if (runChoice == "1")
-			{
-				Console.WriteLine("");
-				Console.WriteLine("Please enter your name: ");
-				name = Console.ReadLine();
-				using (StreamWriter sw = new StreamWriter("Character.txt"))
-				{
-					sw.WriteLine(name);
-					foreach (Song song in songs)
-					{
-						sw.WriteLine(song._songTitle + "," + song._songNotes + "," + song._action + ", " + song._avail);
+        public static void RunMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("Welcome To Ocarina Quest!!");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1) Create new player");
+            Console.WriteLine("2) Continue ");
+            Console.WriteLine("3) Exit");
 
-					}
-				}
-				ContinueMenu();
-			}
-			else if (runChoice == "2")
-			{
-				using (var reader = new StreamReader(contFile))
-				{
-					name = reader.ReadLine();
-				}
-				if (name == "")
-				{
-					Console.Clear();
-					Console.WriteLine("It looks like there are no previous players");
-					RunMenu();
-				}
-				else if (name != "")
-				{
-					
-					ContinueMenu();
-				}
-			}
-			else
-			{
-			}
-		}
+            string runChoice = Console.ReadLine();
 
-		public static void ContinueMenu()
-		{
-			Console.WriteLine("");
-			Console.WriteLine("Welcome {0}!!!", name);
-			Console.WriteLine("What would you like to do?");
-			Console.WriteLine("1) Learn Song");
-			Console.WriteLine("2) List Song");
-
-			string choice1 = Console.ReadLine();
-
-			if (choice1 == "1")
-			{
-
-			}
-			else if (choice1 == "2")
-			{
-				Console.WriteLine("");
-				foreach (Song song in contSongs)
-				{
-					if (song._avail == true)
-					{
-						Console.ForegroundColor = ConsoleColor.Green;
-						Console.WriteLine(song._songTitle + " Learned");
-						Console.ResetColor();
-					}
-                    else
-					{
-						Console.ForegroundColor = ConsoleColor.Red;
-						Console.WriteLine(song._songTitle + " Not Learned");
-						Console.ResetColor();
-					}
+            if (runChoice == "1")
+            {
+                Console.Clear();
+                Console.WriteLine("");
+                Console.WriteLine("Please enter your name: ");
+                name = Console.ReadLine();
+                WriteSongs();
+                contSongs = ReadSongs(songFile);
+                ContinueMenu();
+            }
+            else if (runChoice == "2")
+            {
+                using (var reader = new StreamReader(contFile))
+                {
+                    name = reader.ReadLine();
                 }
-				Console.WriteLine("Would you like to learn any songs? y/n: ");
-			}
+                if (name == "")
+                {
+                    Console.Clear();
+                    Console.WriteLine("It looks like there are no previous players");
+                    RunMenu();
+                }
+                else if (name != "")
+                {
+                    ContinueMenu();
+                }
+            }
+            else if (runChoice == "3")
+            {
+            }
             else
-			{
-				Console.WriteLine("I'm sorry, that isn't a valid option");
-				ContinueMenu();
-			}
-		}
+            {
+                Console.WriteLine("That's not a valid option");
+                RunMenu();
+            }
+        }
 
-        public static void LearnSong(string title)
-		{
+        public static void ContinueMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("Welcome {0}!!!", name);
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1) Learn Song");
+            Console.WriteLine("2) List Song");
 
-		}
+            string choice1 = Console.ReadLine();
 
-		public static List<Song> ReadSongs(string fileName)
-		{
-			var songs = new List<Song>();
-			using (var reader = new StreamReader(fileName))
-			{
-				string line = "";
-				reader.ReadLine();
-				while ((line = reader.ReadLine()) != null)
-				{
-					var song = new Song();
-					string[] values = line.Split(',');
+            if (choice1 == "1")
+            {
+                LearnSong();
+            }
+            else if (choice1 == "2")
+            {
+                Console.Clear();
+                ListSongs(contSongs);
+                Console.WriteLine("Would you like to learn any songs? y/n: ");
+                string learnSong = Console.ReadLine();
+                if (learnSong == "y")
+                {
+                    LearnSong();
+                }
+                else if (learnSong == "n")
+                {
+                    Console.WriteLine("Fair enough! Thanks!");
+                    RunMenu();
+                }
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry, that isn't a valid option");
+                ContinueMenu();
+            }
+        }
 
-					string songTitle = values[0];
-					song._songTitle = songTitle;
+        public static void LearnSong()
+        {
+            Console.Clear();
+            ListSongs(contSongs);
+            Console.WriteLine("Which song?");
+            string title = Console.ReadLine().ToUpper();
+            foreach (Song song in contSongs)
+            {
+                if (title == song._songTitle.ToUpper() && song._avail == false)
+                {
+                    song._avail = true;
+                    Console.Clear();
+                    Console.WriteLine("Awesome! You learned {0}!", song._songTitle);
+                    WriteSongs();
+                    ListSongs(contSongs);
+                    Console.ReadLine();
+                    ContinueMenu();
+                }
+                else if (title == song._songTitle.ToUpper() && song._avail == true)
+                {
+                    Console.WriteLine("It looks like you already know that one!");
+                    Console.ReadLine();
+                    ContinueMenu();
+                }
+            }
+            Console.WriteLine("Something went wrong!!");
+            Console.ReadLine();
+            ContinueMenu();
+                
+            
+        }
 
-					string songNotes = values[1];
-					song._songNotes = songNotes;
+        public static void ListSongs(List<Song> songs)
+        {
+            foreach (Song song in songs)
+            {
+                if (song._avail == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(song._songTitle + " Learned");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(song._songTitle + " Not Learned");
+                    Console.ResetColor();
+                }
+            }
+        }
 
-					string action = values[2];
-					song._action = action;
+        public static void WriteSongs()
+        {
+            using (StreamWriter sw = new StreamWriter("Character.txt"))
+            {
+                sw.WriteLine(name);
+                foreach (Song song in contSongs)
+                {
+                    sw.WriteLine(song._songTitle + "," + song._songNotes + "," + song._action + ", " + song._avail);
+                }
+            }
+        }
 
-					bool avail;
-					if (Boolean.TryParse(values[3], out avail))
-					{
-						song._avail = avail;
-					}
+        public static List<Song> ReadSongs(string fileName)
+        {
+            var songs = new List<Song>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var song = new Song();
+                    string[] values = line.Split(',');
 
-					songs.Add(song);
-				}
-			}
-			return songs;
-		}
+                    string songTitle = values[0];
+                    song._songTitle = songTitle;
 
-	}
+                    string songNotes = values[1];
+                    song._songNotes = songNotes;
+
+                    string action = values[2];
+                    song._action = action;
+
+                    bool avail;
+                    if (Boolean.TryParse(values[3], out avail))
+                    {
+                        song._avail = avail;
+                    }
+
+                    songs.Add(song);
+                }
+            }
+            return songs;
+        }
+
+    }
 }
